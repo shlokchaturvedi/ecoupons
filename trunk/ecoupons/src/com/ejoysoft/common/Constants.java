750d02d795b9ab6bf767b17cecc9129816bb46f4
yangnet@126.com <yangnet@126.com@a1fa9740-cef7-905d-b71b-94a6192708a2>
2011-12-26 00:40:36 +0000

    新增激活充值消费三个字段（杨龙）

--- trunk/ecoupons/src/com/ejoysoft/common/Constants.java
+++ trunk/ecoupons/src/com/ejoysoft/common/Constants.java
@@ -41,6 +41,12 @@ public final class Constants {
     public final static String SEND_STR = "send";
     //����ɾ��
     public final static String DELETE_STR = "delete";
+  //审核
+    public final static String AUDIT_STR = "audit";
+    //充值
+    public final static String RECHARGE_STR = "recharge";
+    //消费
+    public final static String CHARGE_STR = "charge";
     //���ɾ��
     public final static String REMOVE_STR = "remove";
     //�鿴
