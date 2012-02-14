661f2ca89f0209097b704386eb3c8ba4aa3271ab
yangnet@126.com <yangnet@126.com@a1fa9740-cef7-905d-b71b-94a6192708a2>
2012-02-14 13:16:45 +0000

    网站登录

--- trunk/ecoupons/src/com/ejoysoft/common/SessionFilter.java
+++ trunk/ecoupons/src/com/ejoysoft/common/SessionFilter.java
@@ -21,7 +21,7 @@ public class SessionFilter implements Filter {
         }
 	}
 	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
+		FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse rsp = (HttpServletResponse) response;
 		rsp.setContentType("text/html;GBK");
@@ -29,21 +29,38 @@ public class SessionFilter implements Filter {
 		String request_uri = req.getRequestURI();    //得到用户请求的URI
 		String ctxPath = req.getContextPath();
 		UserSession userSession = (UserSession) req.getSession().getAttribute(Constants.USER_KEY);
+		MemberSession membersession = (MemberSession) req.getSession().getAttribute(Constants.MEMBER_KEY);
 		String mHttpUrlName = req.getContextPath();
 		String mServerUrl = "http://" + request.getServerName() + ":"+ request.getServerPort()+ mHttpUrlName; 
-        try {
-            if(userSession == null && (request_uri.indexOf(logon_page)==-1 &&request_uri.indexOf(uncheck)==-1&& request_uri.indexOf(nocheck_page)==-1 && !(ctxPath+"/").equals(request_uri))) {
-                PrintWriter out = rsp.getWriter();
-                out.println("<script type='text/javascript'>alert('登录超时,重新登录！');top.location='"+mServerUrl+"/"+logon_page+"'</script>");
-                return;
-            }else {
-            	chain.doFilter(request, response);
-           }
-       } catch (IOException e) {
-            e.getMessage();
-        } catch (ServletException e1) {
-           e1.getMessage();
-        }
+		if(membersession!=null){
+			chain.doFilter(request, response);
+		}
+			try {
+				
+				if(userSession == null && (request_uri.indexOf(logon_page)==-1 &&request_uri.indexOf(uncheck)==-1&& request_uri.indexOf(nocheck_page)==-1 && !(ctxPath+"/").equals(request_uri))) {
+					
+					PrintWriter out = rsp.getWriter();
+					out.println("<script type='text/javascript'>alert('登录超时,重新登录！');top.location='"+mServerUrl+"/"+logon_page+"'</script>");
+					return;
+				}else if(membersession == null && (request_uri.indexOf(logon_page)==-1 &&request_uri.indexOf(uncheck)==-1&& request_uri.indexOf(nocheck_page)==-1 && !(ctxPath+"/").equals(request_uri))) {
+					PrintWriter out = rsp.getWriter();
+					out.println("<script type='text/javascript'>alert('登录超时,重新登录！');top.location='"+mServerUrl+"/"+logon_page+"'</script>");
+					return;
+				}
+				else {
+					chain.doFilter(request, response);
+				}
+			} catch (IOException e) {
+				e.getMessage();
+			} catch (ServletException e1) {
+				e1.getMessage();
+			}
+		
+		
+		
+		
+		
+		
     }
 
 	public void destroy() {
